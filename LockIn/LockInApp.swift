19f302ff4cf348205c1ba823de3725297f6892eb
Jdanielcode1 <a01284664@itesm.mx>
2025-10-20 13:52:11 -0600

    Initial Commit

---
+++ LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by D Cantu on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
